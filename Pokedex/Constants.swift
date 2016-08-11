36a3c953355374789725a3acfa1359633b6aed7f
yosvanilopez2 <yosvanilopez@Yosvanis-MacBook-Pro.local>
2016-08-11 15:26:51 -0400

    many many changes once again
    still have to fix alamo fire problems

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  
+//
+//  Created by Yosvani Lopez on 8/11/16.
+//
+//
+
+import Foundation
+
+var BaseURL: String = "http://pokeapi.co/api/v2/"
+var PokemonURL: String = "pokemon/" 
--- Pokedex/Pokedex/PokeInfoVC.swift
+++ Pokedex/Pokedex/PokeInfoVC.swift
@@ -8,15 +8,21 @@
 import UIKit
 class PokeInfoVC: UIViewController {
     var pokemon: Pokemon!
-    @IBOutlet weak var name: UILabel! 
+    @IBOutlet weak var name: UILabel!
     @IBOutlet weak var pokeDescription: UILabel!
     @IBOutlet weak var Weight: UILabel!
     @IBOutlet weak var type: UILabel!
     @IBOutlet weak var height: UILabel!
     @IBOutlet weak var pokeId: UILabel!
+    @IBOutlet weak var pokeImg1: UIImageView!
+    @IBOutlet weak var pokeImg2: UIImageView!
+    @IBOutlet weak var pokeImg3: UIImageView!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.name.text = pokemon.name
+        self.pokeImg1.image = UIImage(named: "\(pokemon.pokeId)")
     }
 }
